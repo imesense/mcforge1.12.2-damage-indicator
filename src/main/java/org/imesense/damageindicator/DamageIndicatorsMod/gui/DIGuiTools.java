93a50508f9e21c22b6a84927c24932b48c29e872
OldSerpskiStalker <ictopiar@gmail.com>
2026-03-01 16:03:33 +0500

    Revert

--- src/main/java/org/imesense/damageindicator/DamageIndicatorsMod/gui/DIGuiTools.java
+++ src/main/java/org/imesense/damageindicator/DamageIndicatorsMod/gui/DIGuiTools.java
@@ -591,31 +591,30 @@ public class DIGuiTools extends GuiIngame
 
     public static void renderEntity(EntityLivingBase el)
     {
-        GlStateManager.pushMatrix();
-        GlStateManager.pushAttrib();
-
+        GL11.glDisable(3042);
+        GL11.glEnable(2929);
         try
         {
             float backup = RenderLiving.NAME_TAG_RANGE;
             RenderLiving.NAME_TAG_RANGE = 0.0f;
-
-            Render render = Minecraft.getMinecraft()
-                .getRenderManager()
-                .getEntityRenderObject(el);
-
+            Render render = Minecraft.getMinecraft().getRenderManager().getEntityRenderObject(el);
             if (render != null)
             {
-                render.doRender(el, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+                render.doRender(el, 0.0d, 0.0d, 0.0d, 0.0f, 1.0f);
             }
-
             RenderLiving.NAME_TAG_RANGE = backup;
         }
-        catch (Throwable ignored)
+        catch (Throwable th)
         {
         }
-
-        GlStateManager.popAttrib();
-        GlStateManager.popMatrix();
+        GL11.glEnable(3042);
+        GL11.glClear(256);
+        GL11.glDisable(2929);
+        GL11.glColor4f(255.0f, 255.0f, 255.0f, 255.0f);
+        GL11.glBlendFunc(770, 771);
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GlStateManager.disableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 
     public DIGuiTools(Minecraft par1Minecraft)
